c7f31f7382119c870858fee49e49bee05a522834
David De Backer <davde@microsoft.com>
2025-10-16 21:38:37 -0400

    added user token support for authN

--- Scripts/ManageESUAssignments.ps1
+++ Scripts/ManageESUAssignments.ps1
@@ -14,15 +14,19 @@ This script manages the assignment of ARC based ESU licenses for servers needing
 It retrieves information from a CSV file and the command line for tasks like license assignment and removal.
 Its purpose is to allow you to assign a single license to multiple servers at once or to remove a license from multiple servers at once.
 It supports cross-subscription scenarios where ESU licenses can be located in different subscriptions than the ARC servers.
-Its main targets are servers that are exempted from ESU costs like VMs on Azure VMWare Services or servers that are described in tne following article:
+Its main targets are servers that are exempted from ESU costs like VMs on Azure VMware Services or servers that are described in tne following article:
 https://learn.microsoft.com/en-us/azure/azure-arc/servers/deliver-extended-security-updates#additional-scenarios
+
+The script supports two authentication methods:
+1. Service Principal authentication (requires tenantId, appID, and clientSecret)
+2. User token authentication (requires a valid Microsoft Entra ID authentication token)
 
 .NOTES
 File Name : ManageESUAssignments.ps1
 Author    : David De Backer
-Version   : 1.1
+Version   : 1.2
 Date      : 12-November-2023  
-Update    : 10-October-2025
+Update    : 16-October-2025
 Tested on : PowerShell Version 7.3.8
 Module    : Azure Powershell version 9.6.0
 Requires  : Powershell Core version 7.x or later
@@ -34,6 +38,8 @@ v1.1 - Added support for cross-subscription license assignments. ESU licenses ca
        Added backward compatibility with existing CSV format.
        Added optional -licenseSubscriptionId parameter and LicenseSubscriptionId CSV column.
        CSV LicenseSubscriptionId column always takes precedence over command line parameter when provided.
+v1.2 - Added support for user token authentication. You can now provide a Microsoft Entra ID authentication token instead of service principal credentials.
+       Made tenantId, appID, and clientSecret parameters optional when using token authentication.
 
 .LINK
 To get more information on Azure ARC ESU license REST API please visit:
@@ -56,8 +62,16 @@ https://learn.microsoft.com/en-us/azure/azure-arc/servers/api-extended-security-
 -location "EastUS" `
 -csvFilePath "C:\Temp\ESU Association File.csv"
 
+.EXAMPLE-3
+$authToken = Get-AzAccessToken -ResourceUrl https://management.azure.com/
+./ManageESUAssignments -subscriptionId "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" `
+-location "EastUS" `
+-csvFilePath "C:\Temp\ESU Association File.csv" `
+-userToken $authToken
+
 These examples will assign or unassign (unlink) ESU licenses to/from ARC server objects based on the information provided in the CSV file.
 Example 2 shows how to specify a different subscription for ESU licenses.
+Example 3 shows how to use Microsoft Entra ID token authentication instead of service principal credentials.
 
 You will need to provide the following information in the CSV file:
 LicenseName: The name of the ESU license to used.
@@ -84,15 +98,15 @@ param(
     [Alias("licenseSub")]
     [string]$licenseSubscriptionId,
 
-    [Parameter(Mandatory=$true, HelpMessage="The tenant ID of the Microsoft Entra instance used for authentication.")]
+    [Parameter(Mandatory=$false, HelpMessage="The tenant ID of the Microsoft Entra instance used for authentication.")]
     [ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$', ErrorMessage="The input '{0}' has to be a valid tenant ID.")]
     [string]$tenantId,
 
-    [Parameter(Mandatory=$true, HelpMessage="The application (client) ID as shown under App Registrations that will be used to authenticate to the Azure API.")]
+    [Parameter(Mandatory=$false, HelpMessage="The application (client) ID as shown under App Registrations that will be used to authenticate to the Azure API.")]
     [ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$', ErrorMessage="The input '{0}' has to be a valid application ID.")]
     [string]$appID,
 
-    [Parameter(Mandatory=$true, HelpMessage="A valid (non expired) client secret for App Registration that will be used to authenticate to the Azure API.")]
+    [Parameter(Mandatory=$false, HelpMessage="A valid (non expired) client secret for App Registration that will be used to authenticate to the Azure API.")]
     [Alias("s","secret","sec")]
     [string]$clientSecret,
 
@@ -107,7 +121,11 @@ param(
 
     [Parameter(Mandatory=$false, HelpMessage="The name of the log file to be created.")]
     [Alias("log")]
-    [string]$logFileName
+    [string]$logFileName,
+
+    [Parameter(Mandatory=$false, HelpMessage="The bearer token obtained from the Azure API by the user. If not provided, the script will require the appID, clientSecret and tenantId parameters.")]
+    [Alias("token")]
+    [System.Object]$userToken
 )
 
 #####################################
@@ -146,6 +164,7 @@ function AssignESULicense {
         [string]$ARCServerName,
         [string]$serverResourceGroupName,
         [string]$location,
+        [string]$token,
         [switch]$unassign
     )
 
@@ -153,8 +172,12 @@ function AssignESULicense {
     $licenseID = "/subscriptions/$licenseSubscriptionId/resourceGroups/$licenseResourceGroupName/providers/Microsoft.HybridCompute/licenses/$licenseName" 
     $method = "PUT"
 
-    # Gets a bearer token from the App
-    $bearerToken = Get-AzureADBearerToken -appID $appID -clientSecret $clientSecret -tenantId $tenantId 
+    # Use provided token or get a bearer token from the App
+    if ($token) {
+        $bearerToken = $token
+    } else {
+        $bearerToken = Get-AzureADBearerToken -appID $appID -clientSecret $clientSecret -tenantId $tenantId 
+    }
 
     # Sets the headers for the request
     $headers = @{
@@ -252,6 +275,28 @@ function Write-Logfile  {
 # Main script block #
 #####################
 
+Clear-Host
+# Gets an authorization token either from the user provided one or from the Azure App Registration if one was provided as part of the command line.
+
+# Check if the token is still valid
+if ($userToken) {
+    if ($userToken.ExpiresOn -gt (Get-Date)) {
+        Write-Host "Using provided Microsoft Entra ID authentication token" -ForegroundColor Green
+        #$token = $userToken.Token
+        #Modified $token variable to match the new output format of the Get-AzAccessToken as it changed from a string to a SecureString type
+        $token = ConvertFrom-SecureString -SecureString $userToken.Token -AsPlainText
+    } else {
+        Write-Host "The provided user token has expired. Please provide a valid token.`nExiting." -ForegroundColor Red
+        exit
+    }
+} elseif ($tenantId -and $appID -and $clientSecret) {
+    Write-Host "Getting authentication token from Microsoft Entra ID" -ForegroundColor Green
+    $token = Get-AzureADBearerToken -appID $appID -clientSecret $clientSecret -tenantId $tenantId 
+} else {
+    Write-Host "You need to provide either the tenant, appID and clientSecrets parameters or a valid authentication token object.`nExiting." -ForegroundColor Red
+    exit
+}
+
 Write-Host ""
 Write-Host "=============================================="
 Write-Host "Starting ESU license assignments from CSV file"
@@ -293,6 +338,7 @@ foreach ($row in $data) {
                     'tenantId' = $tenantId
                     'appID' = $appID
                     'clientSecret' = $clientSecret
+                    'token' = $token
                     'licenseResourceGroupName' = $row.licenseResourceGroupName
                     'licenseName' = $row.LicenseName
                     'serverResourceGroupName' = $row.ServerResourceGroupName
@@ -312,6 +358,7 @@ foreach ($row in $data) {
                     'tenantId' = $tenantId
                     'appID' = $appID
                     'clientSecret' = $clientSecret
+                    'token' = $token
                     'licenseResourceGroupName' = $row.licenseResourceGroupName
                     'licenseName' = $row.LicenseName
                     'serverResourceGroupName' = $row.ServerResourceGroupName
